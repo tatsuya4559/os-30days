5bb9c481ad796cb7bb52b9ba014179cd38f09b9f
tatsuya4559 <tatsuya.k1029@gmail.com>
2024-08-03 10:13:37 +0900

    Palette

--- bootpack.c
+++ bootpack.c
@@ -1,9 +1,66 @@
-extern void io_hlt(void);
+extern void _io_hlt(void);
+extern void _write_mem8(int addr, int data);
+extern void _io_cli(void);
+extern void _io_out8(int port, int data);
+extern int _io_load_eflags(void);
+extern void _io_store_eflags(int eflags);
+
+void
+set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = _io_load_eflags(); // load interrupt flag
+    _io_cli(); // set interrupt flag to 0; forbid interrupting
+    _io_out8(0x03c8, start);
+    for (i = start; i <= end; i++) {
+        _io_out8(0x03c9, rgb[0] / 4);
+        _io_out8(0x03c9, rgb[1] / 4);
+        _io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    _io_store_eflags(eflags); // recover interrupt flag
+    return;
+}
+
+void
+init_palette(void)
+{
+    // 普通に初期値を書くと要素数だけ代入文にコンパイルされる
+    // 非効率なのでstatic宣言することで一発で初期化できる
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00, // 0: black
+        0xff, 0x00, 0x00, // 1: light red
+        0x00, 0xff, 0x00, // 2: light green
+        0xff, 0xff, 0x00, // 3: light yellow
+        0x00, 0x00, 0xff, // 4: light blue
+        0xff, 0x00, 0xff, // 5: light purple
+        0x00, 0xff, 0xff, // 6: light cyan
+        0xff, 0xff, 0xff, // 7: white
+        0xc6, 0xc6, 0xc6, // 8: light gray
+        0x84, 0x00, 0x00, // 9: dark red
+        0x00, 0x84, 0x00, // 10: dark green
+        0x84, 0x84, 0x00, // 11: dark yellow
+        0x00, 0x00, 0x84, // 12: dark blue
+        0x84, 0x00, 0x84, // 13: dark purple
+        0x00, 0x84, 0x84, // 14: dark cyan
+        0x84, 0x84, 0x84, // 15: dark gray
+    };
+    set_palette(0, 15, table_rgb);
+    return;
+}
 
 void
 hari_main(void)
 {
-fin:
-    io_hlt();
-    goto fin;
+    int i;
+
+    init_palette();
+
+    for (i = 0xa0000; i <= 0xaffff; i++) {
+        _write_mem8(i, i & 0x0f);
+    }
+
+    for (;;) {
+        _io_hlt();
+    }
 }
--- nasmfunc.asm
+++ nasmfunc.asm
@@ -1,9 +1,77 @@
 ; vim: tabstop=8 noexpandtab
 
 section .text
-	GLOBAL	io_hlt
+	GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+	GLOBAL	_io_in8, _io_in16, _io_in32
+	GLOBAL	_io_out8, _io_out16, _io_out32
+	GLOBAL	_io_load_eflags, _io_store_eflags
+	GLOBAL	_write_mem8
 
-; void io_hlt(void)
-io_hlt:
+_io_hlt: ; void _io_hlt(void);
 	HLT
+	RET
+
+_io_cli: ; void _io_cli(void);
+	CLI
+	RET
+
+_io_sti: ; void _io_sti(void);
+	STI
+	RET
+
+_io_stihlt: ; void _io_stihlt(void);
+	STI
+	HLT
+	RET
+
+_io_in8: ; int _io_in8(int port);
+	MOV	EDX,[ESP+4]	; port
+	MOV	EAX,0
+	IN	AL,DX
+	RET
+
+_io_in16: ; int _io_in16(int port);
+	MOV	EDX,[ESP+4]	; port
+	MOV	EAX,0
+	IN	AX,DX
+	RET
+
+_io_in32: ; int _io_in32(int port);
+	MOV	EDX,[ESP+4]	; port
+	IN	EAX,DX
+	RET
+
+_io_out8: ; void _io_out8(int port, int data);
+	MOV	EDX,[ESP+4]	; port
+	MOV	AL,[ESP+8]	; data
+	OUT	DX,AL
+	RET
+
+_io_out16: ; void _io_out16(int port, int data);
+	MOV	EDX,[ESP+4]	; port
+	MOV	EAX,[ESP+8]	; data
+	OUT	DX,AX
+	RET
+
+_io_out32: ; void _io_out32(int port, int data);
+	MOV	EDX,[ESP+4]	; port
+	MOV	EAX,[ESP+8]	; data
+	OUT	DX,EAX
+	RET
+
+_io_load_eflags: ; int _io_load_eflags(void);
+	PUSHFD
+	POP	EAX	; returnしたときにEAXに入っている値が戻り値
+	RET
+
+_io_store_eflags: ; void _io_store_eflags(int eflags);
+	MOV	EAX,[ESP+4]
+	PUSH	EAX
+	POPFD
+	RET
+
+_write_mem8: ; void _write_mem8(int addr, int data);
+	MOV	ECX,[ESP+4]	; addr
+	MOV	AL,[ESP+8]	; data
+	MOV	[ECX],AL
 	RET
